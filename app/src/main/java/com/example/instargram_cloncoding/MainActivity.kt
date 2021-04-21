589a491e388b279132c5ae9945318c452a9c4a7f
yunsung <12345ggh81@gmail.com>
2021-04-21 21:17:10 +0900

    Update_

--- app/src/main/java/com/example/instargram_cloncoding/MainActivity.kt
+++ app/src/main/java/com/example/instargram_cloncoding/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.instargram_cloncoding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
