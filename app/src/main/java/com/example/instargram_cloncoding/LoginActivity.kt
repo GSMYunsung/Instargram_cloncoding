b72d6819d56195520e5ee03e3079d2469415f3ce
yunsung <12345ggh81@gmail.com>
2021-04-24 23:52:47 +0900

    Login_

--- app/src/main/java/com/example/instargram_cloncoding/LoginActivity.kt
+++ app/src/main/java/com/example/instargram_cloncoding/LoginActivity.kt
@@ -8,10 +8,12 @@ import androidx.databinding.DataBindingUtil
 import com.example.instargram_cloncoding.databinding.ActivityLoginBinding
 import com.google.android.gms.auth.api.Auth
 import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 
 //이메일 로그인 : 이메일을 요청하게되면 파이어베이스에서 요청하는 두단계 구조
 //소셜 로그인 : 구글로그인 -> 파이어베이스 요청 -> 파이어베이스가 구글에 로그인 요청
@@ -34,6 +36,7 @@ class LoginActivity : AppCompatActivity() {
         //로그인 버튼이 눌렸을때 실행
        binding.emailLoginButton.setOnClickListener {
            //구글 로그인 순서 첫번
+           //(1)
             singin_sunginup()
         }
         binding.googleSingInButton.setOnClickListener {
@@ -43,7 +46,7 @@ class LoginActivity : AppCompatActivity() {
         //구글 로그인 인증 토큰
 
         var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id)) // 아이디 인증토큰
+                .requestIdToken(getString(R.string.default_web_client_id)) // 아이디 인증토
                 .requestEmail() // 이메일을 받아올 수 있도록 해줌
                 .build()
         googleSignInClient = GoogleSignIn.getClient(this,gso)
@@ -62,11 +65,29 @@ class LoginActivity : AppCompatActivity() {
             if (result != null) { //null감
                 if(result.isSuccess){
                     var account = result.signInAccount
-                    //FirebaseAuth <- 여기부터 파이어베ㅅ이
+                        //로그인 두번째 순번
+                        //(2)
+                        firebaseAuthWithGoogle(account)
                 }
 
             }
         }
+    }
+
+    //파이어베이스 구글 인증
+    fun firebaseAuthWithGoogle(account: GoogleSignInAccount?) {
+        var credential = GoogleAuthProvider.getCredential(account?.idToken, null)
+        auth?.signInWithCredential(credential)
+                ?.addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        //아이디와 페스워드가 일치하였을 때 실행해줄 코드
+                        moveMainPage(task.result?.user)
+                    } else {
+                        //아이디와 패스워드가 일치하지 않았을때
+                        Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+                    }
+
+                }
     }
 
     //리스너를 설정해줌에따라 아이디와 비밀번호를 받아오고, 값을 받아오기 성공했을때 받아오지못했을때 등 각자의 상황을 실행 (회원가입)
