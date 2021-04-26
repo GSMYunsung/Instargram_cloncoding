b2cf777e6d7095b8882af84b6041f29c09770af7
yunsung <12345ggh81@gmail.com>
2021-04-26 21:12:26 +0900

    Update_

--- app/src/main/java/com/example/instargram_cloncoding/MainActivity.kt
+++ app/src/main/java/com/example/instargram_cloncoding/MainActivity.kt
@@ -1,13 +1,66 @@
 package com.example.instargram_cloncoding
 
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
+import com.example.instargram_cloncoding.databinding.ActivityLoginBinding
+import com.example.instargram_cloncoding.databinding.ActivityMainBinding
+import com.example.instargram_cloncoding.navigation.*
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import java.util.jar.Manifest
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.action_home ->{
+                var detailViewFragment = DetailViewFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content,detailViewFragment).commit()
+                return true
+            }
+            R.id.action_account ->{
+                var gridFragment = GridFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content,gridFragment).commit()
+                return true
+            }
+            R.id.action_add_photo ->{
+                //사진 경로를 가져올 수 있는지 없는지 판별
+                if(ContextCompat.checkSelfPermission(this,android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)
+                startActivity(Intent(this,AddPhotoActivity::class.java))
+                    return true
+            }
+            R.id.action_favorite_alarm ->{
+                var alarmfragment = AlramFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content,alarmfragment).commit()
+                return true
+            }
+            R.id.action_search ->{
+                var userframgnet = UserFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content,userframgnet).commit()
+                return true
+            }
+        }
+        return false
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.bottomNavigation.setOnNavigationItemSelectedListener(this)
+        //사진의 경로를 가져와 줄 수 있는 코드
+        ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),1)
     }
+
 }
