818dcffc3ebdb2ae9eb7487a42222a8a2d517042
yunsung <12345ggh81@gmail.com>
2021-04-29 21:14:13 +0900

    Update_

--- app/src/main/java/com/example/instargram_cloncoding/MainActivity.kt
+++ app/src/main/java/com/example/instargram_cloncoding/MainActivity.kt
@@ -61,6 +61,9 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
         binding.bottomNavigation.setOnNavigationItemSelectedListener(this)
         //사진의 경로를 가져와 줄 수 있는 코드
         ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),1)
+
+        //메인화면이 뜨면 메인화면 프레그먼트가 메인화면으로 뜨게하는 코드
+        binding.bottomNavigation.selectedItemId = R.id.action_home
     }
 
 }
