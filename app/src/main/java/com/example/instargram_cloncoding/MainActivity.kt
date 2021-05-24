1b304f6e90babea43f8c8c61da8581a76218555e
yunsung <12345ggh81@gmail.com>
2021-05-24 21:18:19 +0900

    MainActivity_update

--- app/src/main/java/com/example/instargram_cloncoding/MainActivity.kt
+++ app/src/main/java/com/example/instargram_cloncoding/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.instargram_cloncoding
 
+import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
@@ -13,8 +14,12 @@ import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import com.example.instargram_cloncoding.databinding.ActivityMainBinding
 import com.example.instargram_cloncoding.navigation.*
+import com.google.android.gms.tasks.Task
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.UploadTask
 
 class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
 
@@ -70,9 +75,30 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
         binding.bottomNavigation.setOnNavigationItemSelectedListener(this)
         //사진의 경로를 가져와 줄 수 있는 코드
         ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),1)
-
+        
         //메인화면이 뜨면 메인화면 프레그먼트가 메인화면으로 뜨게하는 코드
         binding.bottomNavigation.selectedItemId = R.id.action_home
+    }
+
+
+    //프로필 사진 넣는곳
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == UserFragment.PICK_PROFILE_FROM_ALBUM && resultCode == Activity.RESULT_OK){
+            var imagUir = data?.data
+            var uid = FirebaseAuth.getInstance().currentUser?.uid
+            //userProfileImages라는 폴더를 생성, 파일이름을 uid로 넣어줌
+            var storegeRef = FirebaseStorage.getInstance().reference.child("userProfileImages").child(uid!!)
+            //이미지를 다운로드 해주기위해!
+            storegeRef.putFile(imagUir!!).continueWithTask { task : Task<UploadTask.TaskSnapshot> ->
+                return@continueWithTask storegeRef.downloadUrl
+            }.addOnSuccessListener { uri ->
+                var map = HashMap<String,Any>()
+                map["image"] = uri.toString()
+                FirebaseFirestore.getInstance().collection("profileImages").document(uid).set(map)
+            }
+        }
     }
 
 }
